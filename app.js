7ce1b85d187ef4e049b66f207906725cd2ab53c6
sachiket <sachiketbehera@gmail.com>
2020-09-29 10:40:44 +0530

    SQL DB added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
+const db = require('./util/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,6 +14,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
